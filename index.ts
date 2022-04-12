92081a60c9c84e9f90a023172db006843e5ebedd
archerax <10404171+archerax@users.noreply.github.com>
2022-04-12 12:52:43 +0100

    Created basic project

---
+++ index.ts
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         ADOS Helper
+// @namespace    https://ados/
+// @version      0.0.1
+// @description  ADOS Helper
+// @author       archerax
+// @match        https://ados/WirelineRnD_Collection/WirelineRnD/_sprints/taskboard/*/WirelineRnD/*/Sprint*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=undefined.ados
+// @grant        none
+// ==/UserScript==
+
+"use strict";
+
+alert("hello world");
