4f3a23cba27f8fccbcbd41676c4fef8fd7c66321
archerax <10404171+archerax@users.noreply.github.com>
2022-04-13 12:59:36 +0100

    Allow report dialog to be closed

--- src/components/ReportDialog.tsx
+++ src/components/ReportDialog.tsx
@@ -1,9 +1,12 @@
 export interface ReportDialogProps {
     open: boolean;
+    onCloseClicked: () => void;
 }
 
 export const ReportDialog = (props: ReportDialogProps) => (
-    <div style={{ display: props.open ? "block" : "none", position: "absolute", inset: 0, backgroundColor: "white" }}>
-
+    <div style={{ display: props.open ? "block" : "none", position: "absolute", inset: 0, backgroundColor: "white", zIndex: 1 }}>
+        <div style={{ height: "48px", borderBottom: "1px solid silver" }}>
+            <a href="#" onClick={props.onCloseClicked} style={{ float: "right", width: "48px", height: "48px", textAlign: "center", lineHeight: "48px", fontSize: "14pt", borderLeft: "1px solid silver", textDecoration: "none" }}>X</a>
+        </div>
     </div>
 );
