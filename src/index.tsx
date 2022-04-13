a6e0254587dd8aa249d6bf1e74b7faa6b606ce4d
archerax <10404171+archerax@users.noreply.github.com>
2022-04-13 12:28:03 +0100

    Moved more code to react

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,15 @@
 import { render } from "preact";
-import { ReportDialog } from "./components/ReportDialog";
+import { App } from "./components/App";
 import { waitForElement } from "./utils";
 
 async function init() {
     const searchHeader = await waitForElement(".expandable-search-header");
 
-    const reportButtonContainer = document.createElement("div");
-    reportButtonContainer.className = "flex";
-
-    const reportButton = document.createElement("button");
-    reportButton.innerText = "Generate Reports";
-    reportButton.style.height = "32px";
-    reportButton.style.margin = "auto 8px";
-    reportButton.onclick = showModal;
-    reportButtonContainer.appendChild(reportButton);
-
     const container = document.createElement("div");
-    reportButtonContainer.appendChild(container);
+    container.className = "flex";
+    render(<App />, container);
 
-    render(<ReportDialog />, container);
-
-    searchHeader.parentElement?.insertBefore(reportButtonContainer, searchHeader);
+    searchHeader.parentElement?.insertBefore(container, searchHeader);
 };
-
-function showModal() {
-    alert("button clicked");
-}
 
 init();
