d50025d24381915f38f8ffbe2edf5222dd25f432
archerax <10404171+archerax@users.noreply.github.com>
2022-04-13 12:46:16 +0100

    Added code to hook into the page at load

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-"use strict";
+import { waitForElement } from "./utils";
 
-alert("hello world");
+async function init() {
+    const searchHeader = await waitForElement(".expandable-search-header");
+
+    const reportButtonContainer = document.createElement("div");
+    reportButtonContainer.className = "flex";
+
+    const reportButton = document.createElement("button");
+    reportButton.innerText = "Generate Reports";
+    reportButton.style.height = "32px";
+    reportButton.style.margin = "auto 8px";
+    reportButton.onclick = showModal;
+    reportButtonContainer.appendChild(reportButton);
+
+    searchHeader.parentElement?.insertBefore(reportButtonContainer, searchHeader);
+};
+
+function showModal() {
+    alert("button clicked");
+}
+
+init();
