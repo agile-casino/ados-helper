40fd94e5e52a97d24ad1293c8ad5659345d22484
archerax <10404171+archerax@users.noreply.github.com>
2022-04-26 16:38:39 +0100

    update report dialog on url changes

--- src/components/ReportDialog.tsx
+++ src/components/ReportDialog.tsx
@@ -1,12 +1,29 @@
 export interface ReportDialogProps {
+    team: string;
+    sprint: string;
     open: boolean;
     onCloseClicked: () => void;
 }
 
-export const ReportDialog = (props: ReportDialogProps) => (
-    <div style={{ display: props.open ? "block" : "none", position: "absolute", inset: 0, backgroundColor: "white", zIndex: 1 }}>
-        <div style={{ height: "48px", borderBottom: "1px solid silver" }}>
-            <a href="#" onClick={props.onCloseClicked} style={{ float: "right", width: "48px", height: "48px", textAlign: "center", lineHeight: "48px", fontSize: "14pt", borderLeft: "1px solid silver", textDecoration: "none" }}>X</a>
-        </div>
-    </div>
-);
+export const ReportDialog = (props: ReportDialogProps) => {
+    if (props.open) {
+        return (
+            <div class="ui-dialog workitem-dialog ui-dialog-legacy full-screen" style={{ zIndex: 10002 }}>
+                <div class="ui-dialog-titlebar">
+                    <button type="button" class="ui-button ui-button-icon-only ui-dialog-titlebar-close" onClick={props.onCloseClicked}>
+                        <span class="ui-button-icon-primary ui-icon ui-icon-closethick"></span>
+                    </button>
+                </div>
+                <div class="work-item-form-main-header" style={{ borderLeftColor: "rgb(0, 156, 204)" }}>
+                    <div class="info-text-wrapper" style={{ fontSize: "large", padding: "0.5em" }}>{props.team} {props.sprint} Reports</div>
+                </div>
+                <div>
+                    Content Here
+                </div>
+            </div>
+        );
+    }
+    else {
+        return null;
+    }
+};
--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,14 @@ container.className = "flex";
 
 render(<App />, container);
 
+let url = window.location.href;
+const urlChangeEvent = new Event("urlChange");
+
 const observer = new MutationObserver((mutations: MutationRecord[]) => {
+    if (window.location.href !== url) {
+        window.dispatchEvent(urlChangeEvent);
+        url = window.location.href;
+    }
     if (!isInDocument(container)) {
         const searchHeader = document.querySelector(".expandable-search-header");
         if (searchHeader) {
