5c413b981e9d8b8fc3daa68962bee53bd292adbc
archerax <10404171+archerax@users.noreply.github.com>
2022-04-26 16:01:39 +0100

    update report dialog on url changes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,34 @@
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 import { ReportDialog } from "./ReportDialog";
 
 export const App = () => {
+    const [url, setUrl] = useState(window.location.href);
     const [dialogOpen, setDialogOpen] = useState(false);
-    return (
-        <>
-            <button onClick={() => setDialogOpen(!dialogOpen)} style={{ height: "32px", margin: "auto 8px" }}>Generate Reports</button>
-            <ReportDialog open={dialogOpen} onCloseClicked={() => setDialogOpen(!dialogOpen)} />
-        </>
-    );
+
+    function onUrlChange() {
+        setUrl(window.location.href);
+    }
+
+    useEffect(() => {
+        window.addEventListener('urlChange', onUrlChange);
+        return () => window.removeEventListener('urlChange', onUrlChange);
+    }, []);
+
+    const matches = url.match(/.+_sprints\/taskboard\/(.+?)\/(.+?)\/(.+?)\/(.+)/);
+
+    console.log(matches);
+
+    if (matches) {
+        const team = decodeURI(matches[3]);
+        const sprint = decodeURI(matches[4]);
+        return (
+            <>
+                <button onClick={() => setDialogOpen(!dialogOpen)} style={{ height: "32px", margin: "auto 8px" }}>Generate Reports</button>
+                <ReportDialog team={team} sprint={sprint} open={dialogOpen} onCloseClicked={() => setDialogOpen(!dialogOpen)} />
+            </>
+        );
+    }
+    else {
+        return null;
+    }
 };
