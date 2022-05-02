31f7f4adb6d421ac2fa04032e20c37a5a3e8c1ff
archerax <10404171+archerax@users.noreply.github.com>
2022-05-02 12:59:39 +0100

    add chart

--- src/components/ReportDialog.tsx
+++ src/components/ReportDialog.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "preact/hooks";
+import uniq from "lodash/uniq";
+import sum from "lodash/sum";
+import { Pie } from "react-chartjs-2";
 import { getIteration } from "../api/iterationsRepository";
 import { getWorkItemDetails, getWorkItems, WorkItemDetailsDto } from "../api/workItemsRepository";
+import "chart.js/auto";
 
 export interface ReportDialogProps {
     collection: string;
@@ -29,6 +33,22 @@ export const ReportDialog = (props: ReportDialogProps) => {
         updateIteration().catch(console.error);
     }, [props.collection, props.project, props.team, props.sprint]);
 
+    const people = uniq(workItems?.map(x => x.fields["System.AssignedTo"]?.displayName));
+    const points = people.map(x => sum(workItems?.filter(y => y.fields["System.AssignedTo"]?.displayName === x).map(z => z.fields["Microsoft.VSTS.Scheduling.Effort"])));
+
+    const data = {
+        labels: people,
+        datasets: [{
+          data: points,
+          backgroundColor: [
+            'rgb(255,  99, 132)',
+            'rgb(54,  162, 235)',
+            'rgb(255, 205, 86)',
+            'rgb(128, 128, 128)'
+          ]
+        }]
+      };
+
     if (props.open) {
         return (
             <div class="ui-dialog workitem-dialog ui-dialog-legacy full-screen" style={{ zIndex: 10002 }}>
@@ -47,6 +67,7 @@ export const ReportDialog = (props: ReportDialogProps) => {
                             <th>WQ/SDR</th>
                             <th>Description</th>
                             <th>Assignee</th>
+                            <th>Size</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -57,11 +78,15 @@ export const ReportDialog = (props: ReportDialogProps) => {
                                     <td></td>
                                     <td>{x.fields["System.Title"]}</td>
                                     <td>{x.fields["System.AssignedTo"]?.displayName}</td>
+                                    <td>{x.fields["Microsoft.VSTS.Scheduling.Effort"]}</td>
                                 </tr>
                             ))
                         }
                     </tbody>
                 </table>
+                <div style={{ width: "300px", height: "300px" }}>
+                    <Pie data={data} options={{ animation: { duration: 0 } }} />
+                </div>
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,13 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: [".tsx", ".ts"],
+    alias: {
+      "react": "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+      "react/jsx-runtime": "preact/jsx-runtime"
+    },
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
     filename: "index.js",
