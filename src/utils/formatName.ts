5e3e4abbe1fbf3a2c89994ff49aba0f4b62c79d1
archerax <10404171+archerax@users.noreply.github.com>
2022-05-19 16:01:44 +0100

    display first names only

--- src/utils/formatName.ts
+++ src/utils/formatName.ts
@@ -2,10 +2,16 @@ export function formatName(name: string): string {
     if (name) {
         name = name.replace(/\s*<.+/, "");
 
-        const match = name.match(/^(\w+), (\w+).*$/)
+        const reverseMatch = name.match(/^(\w+), (\w+).*$/);
+
+        if (reverseMatch) {
+            return `${reverseMatch[2]}`;
+        }
+
+        const match = name.match(/^(\w+) (\w+).*$/);
 
         if (match) {
-            return `${match[2]} ${match[1]}`;
+            return `${match[1]}`;
         }
     }
     return name;
