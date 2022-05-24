8cc9a7e967a1115848bf6c15707cb558240a4765
archerax <10404171+archerax@users.noreply.github.com>
2022-05-24 12:59:37 +0100

    move report code to folder

--- src/domain/reports/ReportGenerator.ts
+++ src/domain/reports/ReportGenerator.ts
@@ -1,9 +1,8 @@
-import cloneDeep from "lodash/cloneDeep";
-import merge from "lodash/merge";
 import sortBy from "lodash/sortBy";
-import { BorderType, CellObject, CellStyle, ExcelDataType, Hyperlink, utils, writeFile } from "xlsx-js-style";
-import { WorkItem } from "./WorkItem";
-import { formatName } from "../utils/formatName";
+import { CellObject, CellStyle, utils, writeFile } from "xlsx-js-style";
+import { WorkItem } from "../WorkItem";
+import { formatName } from "../../utils/formatName";
+import { Cell } from "./Cell";
 
 function getExtraStyles(workItem: WorkItem): CellStyle {
     const result: CellStyle = {
@@ -119,67 +118,4 @@ export function generateReport(collection: string, project: string, team: string
     utils.book_append_sheet(workbook, worksheet, sprint);
 
     writeFile(workbook, `${team} - ${sprint}.xlsx`);
-}
-
-class Cell implements CellObject {
-
-    public static defaultStyle: CellStyle = {
-        font: {
-            name: "Calibri",
-            sz: 11
-        }
-    };
-
-    public l?: Hyperlink;
-    public s: CellStyle = cloneDeep(Cell.defaultStyle);
-    public t: ExcelDataType = "s";
-    public v?: string | number | boolean | Date;
-
-    constructor(content: string | number | boolean | Date) {
-        this.v = content;
-    }
-
-    public alignText({ horizontal }: { horizontal: "left"|"center"|"right" }) {
-        const patch: CellStyle = {
-            alignment: {
-                horizontal: horizontal
-            }
-        };
-        this.s = merge(this.s, patch);
-        return this;
-    }
-
-    public borderBottom({ color, style }: { color: string, style: BorderType }) {
-        const patch: CellStyle = {
-            border: {
-                bottom: {
-                    color: { rgb: color },
-                    style: style
-                }
-            }
-        };
-        this.s = merge(this.s, patch);
-        return this;
-    }
-
-    public font({ size, bold = false }: { size: number, bold?: boolean }) {
-        const patch: CellStyle = {
-            font: {
-                sz: size,
-                bold: bold
-            }
-        };
-        this.s = merge(this.s, patch);
-        return this;
-    }
-
-    public link(href: string) {
-        this.l = { Target: href };
-        return this;
-    }
-
-    public style(patch: CellStyle) {
-        this.s = merge(this.s, patch);
-        return this;
-    }
 }
