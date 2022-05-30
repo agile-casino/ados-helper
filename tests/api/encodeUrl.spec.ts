d03dc0434c868ea7c46ba4dcaf08b9efe9585fe4
archerax <10404171+archerax@users.noreply.github.com>
2022-05-30 12:39:17 +0100

    setup jest

---
+++ tests/api/encodeUrl.spec.ts
@@ -0,0 +1,13 @@
+import { encodeUrl } from "../../src/api/encodeUrl";
+
+test("encodeUrl_WithOneParameter_ReturnsEncodedUrl", () => {
+    const result = encodeUrl("/foo/{0}", ["bar"]);
+
+    expect(result).toBe("/foo/bar");
+});
+
+test("encodeUrl_WithTwoParameters_ReturnsEncodedUrl", () => {
+    const result = encodeUrl("/foo/{0}/{1}", ["bar", "baz"]);
+
+    expect(result).toBe("/foo/bar/baz");
+});
