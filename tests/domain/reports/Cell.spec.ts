43af177c222c8bc904b9cdf055e0e96fb6ab815d
archerax <10404171+archerax@users.noreply.github.com>
2022-06-08 12:16:25 +0100

    set cell data types

---
+++ tests/domain/reports/Cell.spec.ts
@@ -0,0 +1,25 @@
+import { Cell } from "../../../src/domain/reports/Cell";
+
+test("Cell_WhenContentIsString_CellTypeIsString", () => {
+    const cell = new Cell("test");
+
+    expect(cell.t).toBe("s");
+});
+
+test("Cell_WhenContentIsNumber_CellTypeIsNumber", () => {
+    const cell = new Cell(123);
+
+    expect(cell.t).toBe("n");
+});
+
+test("Cell_WhenContentIsBoolean_CellTypeIsBoolean", () => {
+    const cell = new Cell(true);
+
+    expect(cell.t).toBe("b");
+});
+
+test("Cell_WhenContentIsDate_CellTypeIsDate", () => {
+    const cell = new Cell(new Date());
+
+    expect(cell.t).toBe("d");
+});
