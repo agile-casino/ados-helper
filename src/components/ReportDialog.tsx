93dbfe975f2c2e955390feda61f47081f012e8f7
archerax <10404171+archerax@users.noreply.github.com>
2024-04-21 19:12:35 +0000

    Update packages to fix modal positioning

--- src/components/ReportDialog.tsx
+++ src/components/ReportDialog.tsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from "react";
 import { WorkItemTable } from "./WorkItemTable";
-import { WorkItemChart } from "./WorkItemChart";
 import { QueryClient } from "../api/query/QueryClient";
 import { generateReport } from "../domain/reports/ReportGenerator";
 import { WorkItem } from "../domain/WorkItem";
-import { If } from "./If";
 import { WorkItemClient } from "../api/workItems/WorkItemClient";
 import { ApiClient } from "../api/ApiClient";
 import { Button, Dialog, Title } from "@mantine/core";
@@ -23,20 +21,20 @@ export const ReportDialog = (props: ReportDialogProps) => {
 
     const [workItems, setWorkItems] = useState<WorkItem[]>([]);
 
-    async function updateIteration() {
-        if (props.collection && props.project && props.team && props.sprint) {
-            const workItemClient = new WorkItemClient(origin);
-            const queryClient = new QueryClient(origin, workItemClient);
-            const apiClient = new ApiClient(queryClient, workItemClient);
-
-            const queryResult = await apiClient.getIteration(props.collection, props.project, props.team, props.sprint);
-
-            setWorkItems(queryResult);
-        }
-    }
-
+    
     useEffect(() => {
-        updateIteration().catch(console.error);
+        async function updateIteration() {
+            if (props.collection && props.project && props.team && props.sprint) {
+                const workItemClient = new WorkItemClient(origin);
+                const queryClient = new QueryClient(origin, workItemClient);
+                const apiClient = new ApiClient(queryClient, workItemClient);
+    
+                const queryResult = await apiClient.getIteration(props.collection, props.project, props.team, props.sprint);
+    
+                setWorkItems(queryResult);
+            }
+        }
+        updateIteration().catch((e: unknown) => console.error(e));
     }, [props.collection, props.project, props.team, props.sprint, props.open]);
 
     if (props.open) {
@@ -50,11 +48,6 @@ export const ReportDialog = (props: ReportDialogProps) => {
                         <WorkItemTable origin={props.origin} collection={props.collection} project={props.project} workItems={workItems} />
                         <Button style={{ marginLeft: "1em", marginBottom: "1em" }} onClick={() => generateReport(origin, props.collection, props.project, props.team, props.sprint, workItems)}>Generate Report</Button>
                     </div>
-                    <If condition={!!window.localStorage.getItem("debug")}>
-                        <div style={{ float: "right", position: "relative", height: "300px", width: "400px" }}>
-                            <WorkItemChart workItems={workItems} />
-                        </div>
-                    </If>
                 </div>
             </Dialog>
         );
