30e6178bb41d7a9484994dd18908da70d3543686
archerax <10404171+archerax@users.noreply.github.com>
2025-08-30 06:51:20 +0100

    biome

--- tests/domain/reports/Cell.spec.ts
+++ tests/domain/reports/Cell.spec.ts
@@ -1,26 +1,26 @@
-import { test, expect } from "vitest";
+import { expect, test } from "vitest";
 import { Cell } from "../../../src/domain/reports/Cell";
 
 test("Cell_WhenContentIsString_CellTypeIsString", () => {
-    const cell = new Cell("test");
+  const cell = new Cell("test");
 
-    expect(cell.t).toBe("s");
+  expect(cell.t).toBe("s");
 });
 
 test("Cell_WhenContentIsNumber_CellTypeIsNumber", () => {
-    const cell = new Cell(123);
+  const cell = new Cell(123);
 
-    expect(cell.t).toBe("n");
+  expect(cell.t).toBe("n");
 });
 
 test("Cell_WhenContentIsBoolean_CellTypeIsBoolean", () => {
-    const cell = new Cell(true);
+  const cell = new Cell(true);
 
-    expect(cell.t).toBe("b");
+  expect(cell.t).toBe("b");
 });
 
 test("Cell_WhenContentIsDate_CellTypeIsDate", () => {
-    const cell = new Cell(new Date());
+  const cell = new Cell(new Date());
 
-    expect(cell.t).toBe("d");
+  expect(cell.t).toBe("d");
 });
