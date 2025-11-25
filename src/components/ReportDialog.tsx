8abb711314161a7b73981f4ca3bd8d45700c51cf
archerax <10404171+archerax@users.noreply.github.com>
2025-11-25 17:11:37 +0000

    linting

--- src/components/ReportDialog.tsx
+++ src/components/ReportDialog.tsx
@@ -17,9 +17,7 @@ export const ReportDialog = (props: ReportDialogProps) => {
     return (
       <Dialog opened={true} w={1000} h={650} withCloseButton={true} onClose={props.onCloseClicked}>
         <Title order={4} fw={400} style={{ marginBottom: "1rem" }}>
-          <span>
-            {props.sprint} Reports
-          </span>
+          <span>{props.sprint} Reports</span>
         </Title>
         <Tabs defaultValue="current-team">
           <Tabs.List>
@@ -28,23 +26,11 @@ export const ReportDialog = (props: ReportDialogProps) => {
           </Tabs.List>
 
           <Tabs.Panel value="current-team" pt="xs" style={{ height: "calc(650px - 120px)", overflow: "hidden" }}>
-            <CurrentTeamTab
-              origin={props.origin}
-              collection={props.collection}
-              project={props.project}
-              team={props.team}
-              sprint={props.sprint}
-            />
+            <CurrentTeamTab origin={props.origin} collection={props.collection} project={props.project} team={props.team} sprint={props.sprint} />
           </Tabs.Panel>
 
           <Tabs.Panel value="multi-team" pt="xs" style={{ height: "calc(650px - 120px)", overflow: "hidden" }}>
-            <MultiTeamTab
-              origin={props.origin}
-              collection={props.collection}
-              project={props.project}
-              currentTeam={props.team}
-              sprint={props.sprint}
-            />
+            <MultiTeamTab origin={props.origin} collection={props.collection} project={props.project} currentTeam={props.team} sprint={props.sprint} />
           </Tabs.Panel>
         </Tabs>
       </Dialog>
